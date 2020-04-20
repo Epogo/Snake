8c4717e5cf35728f50030324a0e42385d515c7d0
Epogo <62990985+Epogo@users.noreply.github.com>
2020-04-20 14:28:23 +0300

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,157 +1,141 @@
+//initialize relevant variables
+
 var canvas;
 var ctx;
-
+//snakes parts
 var head;
 var apple;
-var ball;
+var body;
 
 var dots;
-var apple_x;
-var apple_y;
+var apple_x, apple_y;
+//initialize directions 
+var leftDirection=true;
+var rightDirection=false;
+var upDirection=false;
+var downDirection=false;
+//is the user in the game currently?
+var inGame=true;
 
-var leftDirection = false;
-var rightDirection = true;
-var upDirection = false;
-var downDirection = false;
-var inGame = true;    
+const Dot_size=10;
+const All_Dots=1600;
+const MAX_RAND=50;
 
-const DOT_SIZE = 10;
-const ALL_DOTS = 900;
-const MAX_RAND = 29;
-const DELAY = 140;
-const C_HEIGHT = 300;
-const C_WIDTH = 300;    
+const C_HEIGHT=400;
+const C_WIDTH=400;
 
-const LEFT_KEY = 37;
-const RIGHT_KEY = 39;
-const UP_KEY = 38;
-const DOWN_KEY = 40;
+const LEFT_KEY=37;
+const RIGHT_KEY=39;
+const UP_KEY=38;
+const DOWN_KEY=40;
+var DELAY;
 
-var x = new Array(ALL_DOTS);
-var y = new Array(ALL_DOTS);   
+var x=new Array(All_Dots);
+var y=new Array(All_Dots);
 
-function init() {
-    
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext('2d');
-
-    loadImages();
-    createSnake();
-    locateApple();
-    setTimeout("gameCycle()", DELAY);
-}    
-
-function loadImages() {
-    
-    head = new Image();
-    head.src = 'head.png';    
-    
-    ball = new Image();
-    ball.src = 'dot.png'; 
-    
-    apple = new Image();
-    apple.src = 'apple.png'; 
+//initalization of gaming board
+if(document.getElementById("easy").checked)
+  DELAY=500;
+if(document.getElementById("moderate").checked)
+DELAY=300;
+if(document.getElementById("Hard").checked)
+DELAY=150;
+function init(){
+  canvas=document.getElementById('myCanvas');
+  ctx=canvas.getContext('2d');
+  
+  LoadImages();
+  GenerateSnake();
+  LocateApple();
+  setTimeout("gameCycle()", DELAY);
 }
 
-function createSnake() {
+function LoadImages(){
+  head=new Image();
+  head.src='head.png';
+  
+  body=new Image();
+  body.src='ball.png';
+  
+  apple=new Image();
+  body.src='apple.png';
+}
 
-    dots = 3;
+function GenerateSnake(){
+  
+  dots=3;
+  for (var i=0;i<dots;i++){
+    x[i]=70-i*10;
+    y[i]=70;
+  }
+  
+}
 
-    for (var z = 0; z < dots; z++) {
-        x[z] = 50 - z * 10;
-        y[z] = 50;
+function checkApple(){
+  
+  if((x[0]==apple_x)&&(y[0]==apple_y)){
+    dots++;
+    LocateApple();
+  }
+ 
+function doDrawing(){
+  ctx.clearRect(0,0,C_WIDTH,C_HEIGHT);
+  
+  if(inGame){
+    ctx.drawImage(apple,apple_x,apple_y);
+    
+    for(var i=0;i<dots;i++){
+      
+      if(z==0){
+        ctx.drawImage(head,x[i],y[i]);
+      }
+      else{
+        ctx.drawImage(body,x[i],y[i]);
+      }
     }
-}
+  }
+ else{
+   gameOver();
+ }
+}  
+
+function gameOver(){
+ ctx.fillStyle='blue';
+ ctx.textBaseline='middle';
+ ctx.textAlign='center';
+ ctx.font='normal bold 18px serif';
+ 
+    ctx.filltext('Game Is Over!,C_WIDTH_2,C_HEIGHT/2');
+  }
 
 function checkApple() {
+  if((x[0]==apple_x)&&(y[0]==apple_y)){
+    dots++;
+    LocateApple();
+  }
+}  
 
-    if ((x[0] == apple_x) && (y[0] == apple_y)) {
+function move(){
+  for (var i=dots;i>0;i--){
+    x[i]=x[(i-1)];
+    y[i]=y[(i-1)];
+  }
 
-        dots++;
-        locateApple();
-    }
-}    
-
-function doDrawing() {
-    
-    ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
-    
-    if (inGame) {
-
-        ctx.drawImage(apple, apple_x, apple_y);
-
-        for (var z = 0; z < dots; z++) {
-            
-            if (z == 0) {
-                ctx.drawImage(head, x[z], y[z]);
-            } else {
-                ctx.drawImage(ball, x[z], y[z]);
-            }
-        }    
-    } else {
-
-        gameOver();
-    }        
-}
-
-function gameOver() {
-    
-    ctx.fillStyle = 'white';
-    ctx.textBaseline = 'middle'; 
-    ctx.textAlign = 'center'; 
-    ctx.font = 'normal bold 18px serif';
-    
-    ctx.fillText('Game over', C_WIDTH/2, C_HEIGHT/2);
-}
-
-function checkApple() {
-
-    if ((x[0] == apple_x) && (y[0] == apple_y)) {
-
-        dots++;
-        locateApple();
-    }
-}
-
-function move() {
-
-    for (var z = dots; z > 0; z--) {
-    
-        x[z] = x[(z - 1)];
-        y[z] = y[(z - 1)];
-    }
-
-    if (leftDirection) {
-    
-        x[0] -= DOT_SIZE;
-    }
-
-    if (rightDirection) {
-    
-        x[0] += DOT_SIZE;
-    }
-
-    if (upDirection) {
-    
-        y[0] -= DOT_SIZE;
-    }
-
-    if (downDirection) {
-    
-        y[0] += DOT_SIZE;
-    }
-}    
-
-function checkCollision() {
-
-    for (var z = dots; z > 0; z--) {
-
-        if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
-            inGame = false;
-        }
-    }
-
-    if (y[0] >= C_HEIGHT) {
+  if(leftDirection)
+    x[0]-=Dot_size;
+  if(rightDirection)
+    x[0]+=Dot_size;
+  if(upDirection)
+    y[0]-=Dot_size;
+  if(downDirection)
+    y[0]+=Dot_size;  
+}  
+  function isCollision(){
+for (var i=dots;i>0;i--){
+  if((z>4)&&(x[0]==x[i])&&(y[0]==y[i]))
+    inGame=false;
+  }
+if (y[0] >= C_HEIGHT) {
     
         inGame = false;
     }
@@ -172,28 +156,25 @@ function checkCollision() {
     }
 }
 
-function locateApple() {
-
-    var r = Math.floor(Math.random() * MAX_RAND);
-    apple_x = r * DOT_SIZE;
-
-    r = Math.floor(Math.random() * MAX_RAND);
-    apple_y = r * DOT_SIZE;
-}    
-
-function gameCycle() {
-    
+function LocateApple(){
+  var r=Math.floor(Math.random()*MAX_RAND);
+apple_x=r*DOT_SIZE;
+r=Math.floor(Math.random()*Max_RAND);
+  apple_y=r*DOT_SIZE;
+}
+  
     if (inGame) {
 
         checkApple();
-        checkCollision();
+        isCollision();
         move();
         doDrawing();
         setTimeout("gameCycle()", DELAY);
-    }
+    }  
+
 }
 
-onkeydown = function(e) {
+onkeyptress = function(e) {
     
     var key = e.keyCode;
     
